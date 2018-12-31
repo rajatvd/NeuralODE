9fdb8c45dc070eeb0d87158410a69b998283015b
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2018-12-31 20:05:48 +0530

    update test to use new import_source

--- test.py
+++ test.py
@@ -2,11 +2,9 @@
 # %%
 import torch
 from sacred import Experiment
-from pytorch_utils.sacred_trainer import read_config, get_model_path
+from pytorch_utils.sacred_utils import read_config, get_model_path, import_source
 from training_functions import validate
 
-from model_ingredient import make_model
-from data_ingredient import make_dataloaders
 
 # %%
 ex = Experiment('test_mnist')
@@ -17,27 +15,38 @@ def input_config():
     run_dir = 'runs/ODEMnistClassification/8'
     epoch = 'latest'
     device = 'cpu'
+    min_end_time = 10
+    max_end_time = 100
 
 @ex.automain
 def main(run_dir,
          epoch,
          device,
+         min_end_time,
+         max_end_time,
          _log):
 
     config = read_config(run_dir)
     _log.info(f"Read config from {run_dir}")
 
-    model = make_model(**{**config['model'], 'device':device}, _log=_log)
+    model_ing = import_source(run_dir, "model_ingredient")
+    model = model_ing.make_model(**{**config['model'], 'device':device}, _log=_log)
     path = get_model_path(run_dir, epoch)
     model.load_state_dict(torch.load(path))
     model = model.eval()
     _log.info(f"Loaded state dict from {path}")
 
-    dset, tl, vl, test_loader = make_dataloaders(**{**config['dataset'],
-                                                    'device':device},
-                                                 _log=_log)
+    if hasattr(model, "odeblock"):
+        _log.info(f"Updated times to {[min_end_time, max_end_time]}")
+        model.odeblock.min_end_time = min_end_time
+        model.odeblock.max_end_time = max_end_time
+
+
+    data_ing = import_source(run_dir, "data_ingredient")
+    dset, tl, vl, test_loader = data_ing.make_dataloaders(**{**config['dataset'],
+                                                             'device':device},
+                                                          _log=_log)
     _log.info("Testing model...")
     test_loss, test_acc = validate(model, test_loader)
 
     _log.info(f"Test loss = {test_loss:.6f}, Test accuracy = {test_acc:.4f}")
-
